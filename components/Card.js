079c7c89accc41cb6ebeab95a7030c1aa3394711
Kushagra Gour <chinchang457@gmail.com>
2021-03-21 22:34:51 +0530

    initial commit

--- components/Card.js
+++ components/Card.js
@@ -1,7 +1,11 @@
-export const Card = ({ children, heading }) => {
+export const Card = ({ children, heading, anchor }) => {
   return (
-    <section className="bg-white shadow-xl p-10 rounded-2xl">
-      {heading ? <h2 className="text-5xl mb-8">{heading}</h2> : null}
+    <section className="bg-white shadow-xl p-10 rounded-2xl border-gray-100 border">
+      {heading ? (
+        <h2 id={anchor} className="text-5xl mb-8">
+          {heading}
+        </h2>
+      ) : null}
 
       {children}
     </section>
