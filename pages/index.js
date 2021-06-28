79aa7af2513fe7d97a4ec5eb9d008b024ddad4a3
Kushagra Gour <chinchang457@gmail.com>
2021-06-28 10:31:58 +0530

    ui

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
       </Head>
 
       <main className="flex-1 max-w-6xl w-full px-4">
-        <h2 className="text-7xl mb-7">Let's Learn English</h2>
+        <h2 className="text-7xl mb-6">Let's Learn English</h2>
         <p className="text-xl leading-normal mb-10">
           An online institute to learn English for Bank / SSC / CAT / DSSSB /
           UPSC / LIC / INSURANCE / CTET / DEFENCE / CLAT and State PSC
